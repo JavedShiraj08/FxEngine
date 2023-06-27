98ed2f6a49d5bf8582d8e6433de1010e60c2d4b7
JavedShiraj08 <34921020+JavedShiraj08@users.noreply.github.com>
2023-06-27 07:30:18 +0530

    Source files and Input and Output data

---
+++ src/main/com/refinitivengine/tcs/Match.scala
@@ -0,0 +1,3 @@
+package com.refinitivengine.tcs
+
+case class Match(orderId1: Int, orderId2: Int, matchTime: Long, quantity: Int, price: Int)
---
+++ src/main/com/refinitivengine/tcs/Order.scala
@@ -0,0 +1,3 @@
+package com.refinitivengine.tcs
+
+case class Order(orderId: Int, user: String, orderTime: Long, orderType: String, quantity: Int, price: Int)
